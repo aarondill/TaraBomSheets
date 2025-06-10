c5c29f687fb3f9b63c662d81121df0e78dda09fb
Aaron Dill <aaronsacks2006@gmail.com>
2025-06-09 20:03:13 -0500

    feat: done

--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,11 @@
+import { stringify } from "csv";
+import { parse } from "csv/sync";
+import { PathLike } from "fs";
+import fs from "fs/promises";
+import { createWriteStream } from "node:fs";
+import { Result } from ".";
+
+export type SourceItem = Data["allItems"][number];
 /**
  * This represents the aggregated data from the spreadsheets
  */
@@ -8,12 +16,11 @@ export interface Data {
 		DESCRIPTION: string;
 	}[];
 	routeStagesAndResources: {
-		// Index as "ITEM GROUP" + " ASSY" or "ITEM GROUP" + " MFG"
-		[itemGroup: string]: {
-			ItemCode: string;
-			Quantity: number | undefined; // if it has a quantity, it's a Resource, otherwise it's a RouteStage
-		}[];
-	};
+		// "ITEM GROUP" + " ASSY" or "ITEM GROUP" + " MFG"
+		"Item Groups - Type": string; // search for all that match this
+		ItemCode: string;
+		Quantity: string | ""; // if it has a quantity, it's a Resource, otherwise it's a RouteStage
+	}[];
 	ittItems: {
 		ParentKey: string;
 		ItemCode: string;
@@ -21,6 +28,44 @@ export interface Data {
 	}[];
 }
 
-export function setup(): Data {
-	throw new Error("Not implemented");
+async function readCSV(
+	path: PathLike
+): Promise<Record<string, string | undefined>[]> {
+	const content = await fs.readFile(path, "utf8");
+	return parse(content, {
+		columns: true,
+		skip_empty_lines: true,
+	}) as Record<string, string>[];
+}
+
+export async function getData(): Promise<Data> {
+	const [allItems, routeStagesAndResources, ittItems] = await Promise.all([
+		readCSV("all-items.csv") as Promise<Data["allItems"]>,
+		readCSV("route-stages-and-resources.csv") as Promise<
+			Data["routeStagesAndResources"]
+		>,
+		readCSV("itt-items.csv") as Promise<Data["ittItems"]>,
+	]);
+	return { allItems, routeStagesAndResources, ittItems };
+}
+
+export async function outputResults(result: Result[]): Promise<void> {
+	const writableStream = createWriteStream("output.csv");
+	const stringifier = stringify({ header: true }).pipe(writableStream);
+	return new Promise((resolve, reject) => {
+		writableStream.on("finish", resolve);
+		writableStream.on("error", reject);
+		for (const lineItem of result) {
+			const [firstStage, firstResource, ...restStages] = lineItem.stages;
+			let lineNum = 1;
+			stringifier.write({ ...firstStage, lineNum: lineNum++ });
+			stringifier.write({ ...firstResource, lineNum: lineNum++ });
+			for (const item of lineItem.items) {
+				stringifier.write({ ...item, lineNum: lineNum++ });
+			}
+			for (const stage of restStages) {
+				stringifier.write({ ...stage, lineNum: lineNum++ });
+			}
+		}
+	});
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { setup } from "./data";
+import { getData, outputResults, SourceItem } from "./data";
 /** TODO:
  *
  */
@@ -7,7 +7,7 @@ interface LineItem {
 	// treecode
 	ParentKey: string;
 	ItemCode: string;
-	Quanity?: number; // RouteStages don't have this
+	Quantity: string | ""; // RouteStages don't have this
 	Warehouse: "040";
 	ItemType: "Route" | "pit_Resource" | "pit_Item";
 	// LineNum counts from 1
@@ -23,29 +23,76 @@ interface LineItem {
  * stages[n][0]
  * stages[n][1]
  */
-interface Result {
+export interface Result {
 	items: Item[];
-	stages: [RouteStage, Resource][];
+	stages: (RouteStage | Resource)[];
 }
 interface Item extends LineItem {
-	quantity: number;
+	Quantity: string;
+	ItemType: "pit_Item";
 }
 interface Resource extends LineItem {
-	quantity: number;
+	Quantity: string;
+	ItemType: "pit_Resource";
 }
 interface RouteStage extends LineItem {
-	quantity: undefined;
+	Quantity: "";
+	ItemType: "Route";
 }
 
-function run(treeCode: number): Result {
-	const isAssembly = false; // TODO: Guess from the desc and the itemcode
-	throw new Error("Not implemented");
+// Guess from the desc and the itemcode
+function probablyIsAssembly(itemInfo: SourceItem, subitems: Item[]): boolean {
+	// if desc contains "ASSY"
+	if (itemInfo.DESCRIPTION.match(/\bASSY\b/) != null) return true;
+	// if itemcode starts with "AP"
+	if (subitems.some(item => item.ItemCode.startsWith("AP"))) return true;
+	return false;
 }
 
-const data = setup();
-/// TODO: for each treecode, run the function
-// for (const treecode of data) {
-// const result = run(data.treeCode);
-// console.log(result);
-// }
-//
+function getItems(itemInfo: SourceItem): Item[] {
+	const items = data.ittItems.filter(
+		item => item.ParentKey === itemInfo["PN#"]
+	);
+	return items.map(item => ({
+		Quantity: item.Quanity,
+		ParentKey: item.ParentKey,
+		ItemCode: item.ItemCode,
+		Warehouse: "040",
+		ItemType: "pit_Item",
+	}));
+}
+
+function getResourcesAndStages(
+	itemInfo: SourceItem,
+	isAssembly: boolean
+): (RouteStage | Resource)[] {
+	const key = itemInfo["ITEM GROUP"] + " " + (isAssembly ? "ASSY" : "MFG");
+	const resources = data.routeStagesAndResources.filter(
+		item => item["Item Groups - Type"] === key
+	);
+	if (resources.length === 0) throw new Error("No resources found: " + key);
+	if (resources.length > 1) throw new Error("Too many resources found: " + key);
+	return resources.map(resource => {
+		const Quantity = resource.Quantity,
+			ParentKey = itemInfo["PN#"],
+			ItemCode = resource.ItemCode,
+			Warehouse = "040";
+		return Quantity === ""
+			? { Quantity, ParentKey, ItemCode, Warehouse, ItemType: "Route" }
+			: { Quantity, ParentKey, ItemCode, Warehouse, ItemType: "pit_Resource" };
+	});
+}
+
+function run(source: SourceItem): Result {
+	const items = getItems(source);
+	const isAssembly = probablyIsAssembly(source, items);
+	const resourcesAndStages = getResourcesAndStages(source, isAssembly);
+	return {
+		items,
+		stages: resourcesAndStages,
+	};
+}
+
+const data = await getData();
+const results = data.allItems.map(run);
+await outputResults(results);
